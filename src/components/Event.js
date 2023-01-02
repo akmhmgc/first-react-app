ac1d4aec0725fccbfa05dadedcd9c6f6999f63d6
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-02 22:45:43 +0900

    style: CSS moduleの適用

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./Event.module.css";
 export default function Event({ index, id, title, handleClick }) {
   return (
-    <>
+    <div className={styles.card}>
       <h2>
         {index}-{title}
       </h2>
       <button onClick={() => handleClick(id)}>Delete Button</button>
-    </>
+    </div>
   );
 }
