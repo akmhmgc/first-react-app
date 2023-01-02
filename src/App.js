7130ab9ee070a3fe64b64b2c97510568789f4637
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-02 18:03:51 +0900

    feat: boolean stateを利用して表示・非表示を切り替える

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [showEvents, setshowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
     { title: "race on moo mo farm", id: 3 },
   ]);
   const handleClick = (id) => {
-    setEvents((prevEvents)=> prevEvents.filter((event)=> event.id !== id))
+    setEvents((prevEvents) => prevEvents.filter((event) => event.id !== id));
   };
+
   return (
     <div className="App">
-      {events.map((event, index) => (
-        <div key={event.id}>
-          <h2>{index}-{event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => setshowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setshowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index}-{event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
