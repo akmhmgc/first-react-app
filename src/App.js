49ef0d05c168c8e49d2c563e79accff3a4240517
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-02 17:28:40 +0900

    feat: Stateを使用して状態を保持する

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("mario");
+  const [events, setEvents] = useState([
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: "race on moo mo farm", id: 3 },
+  ]);
+  const changeName = () => {
+    setName("luigi");
+    console.log(name);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
+      <button onClick={changeName}>Change name</button>
     </div>
   );
 }
