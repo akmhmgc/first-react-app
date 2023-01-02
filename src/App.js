f3066a2552f7d359caf87d8eb248921dd7c4713a
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-02 17:45:23 +0900

    feat: 削除機能の追加

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("mario");
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
     { title: "race on moo mo farm", id: 3 },
   ]);
-  const changeName = () => {
-    setName("luigi");
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents)=> prevEvents.filter((event)=> event.id !== id))
   };
   return (
     <div className="App">
-      <h1>{name}</h1>
-      {events.map((event) => (
+      {events.map((event, index) => (
         <div key={event.id}>
-          <h2>{event.title}</h2>
+          <h2>{index}-{event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
         </div>
       ))}
-      <button onClick={changeName}>Change name</button>
     </div>
   );
 }
